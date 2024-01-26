5e70fbce9178cb86c635047bd86b3d4dcbc0a3de
fawwazabrials <18221067@std.stei.itb.ac.id>
2024-01-26 21:04:24 +0700

    chore: setup project

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+	return (
+		<div className="p-8">
+			<h1 className="text-xl font-semibold">Warteg ITB FRS Scheduler</h1>
+			<p className="italic">Berlokasi di Warteg Selera Sekeloa</p>
+		</div>
+	);
+}
+
+export default Home;
