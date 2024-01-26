f27be0b7e9809a85c48705a3d173ea0853cbe891
fawwazabrials <18221067@std.stei.itb.ac.id>
2024-01-26 21:58:43 +0700

    feat: add header ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
+import Header from "../components/Header";
+
 function Home() {
 	return (
-		<div className="p-8">
-			<h1 className="text-xl font-semibold">Warteg ITB FRS Scheduler</h1>
-			<p className="italic">Berlokasi di Warteg Selera Sekeloa</p>
+		<div className="min-h-screen px-8 py-4">
+			<Header />
 		</div>
 	);
 }
